75a1837914d24750e832b339fe6217c6ef0011e0
Seoha95 <serha95@naver.com>
2023-11-25 10:12:00 +0900

    Feat: 게임 시작 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import baseball.controller.GameController;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+
+		GameController gameController = new GameController(new InputView(), new OutputView());
+
+		gameController.run();
+	}
 }
